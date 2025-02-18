514a3875e19a2faee849f27103a4bf9a3c4ffcf4
Lucas <zubiarrainlucas@gmail.com>
2025-02-18 09:29:53 +0100

    test

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from typing import List
 import uvicorn
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
+import os 
+
 
 # Initialisation de la connexion à Azure Key Vault
 key_vault_name = "secret-crm"
@@ -89,4 +91,5 @@ async def get_clients():
 
 # Lancement du serveur avec Uvicorn
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))  # Récupère le port défini par Azure ou 8000 par défaut
+    uvicorn.run(app, host="0.0.0.0", port=port)
